c93d7d9bbd68a0c03d036078bbe6abca283ab7b4
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2023-02-22 17:24:22 +0100

    Ping

--- src/test/kotlin/social/network/SocialNetworkConsoleTest.kt
+++ src/test/kotlin/social/network/SocialNetworkConsoleTest.kt
@@ -1,16 +1,24 @@
 package social.network
 
+import io.mockk.mockk
+import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class SocialNetworkConsoleTest {
 
+    private val timelines = mockk<PersonalTimelines>(relaxed = true)
+
+    private val console = SocialNetworkConsole(timelines)
+
     @Test
     fun `should post a message to a personal timeline`() {
-
-        val result = console.
+        val result = console.submitCommand("Alice -> I love the weather today")
 
         assertThat(result).isEqualTo(Unit)
+        verify {
+            timelines.publish(User("Alice"), Message("I love the weather today"))
+        }
     }
 
 }
