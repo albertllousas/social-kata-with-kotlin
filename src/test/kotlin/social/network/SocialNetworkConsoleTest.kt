785d8dcafe9136eac19647f2e25a4d4e02874da7
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2023-02-22 17:18:03 +0100

    Add readme

---
+++ src/test/kotlin/social/network/SocialNetworkConsoleTest.kt
@@ -0,0 +1,16 @@
+package social.network
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class SocialNetworkConsoleTest {
+
+    @Test
+    fun `should post a message to a personal timeline`() {
+
+        val result = console.
+
+        assertThat(result).isEqualTo(Unit)
+    }
+
+}
